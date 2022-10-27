7144cf752bbb259369f84b8b5a7d491e9145d0f3
Zenith <93628284+Zenith4@users.noreply.github.com>
2022-10-26 20:07:32 -0500

    Create jumpscare.py

---
+++ jumpscare.py
@@ -0,0 +1,30 @@
+import time,sys,random
+#function I use to write text letter by letter
+def sprint(str,ptime=1,ctime=.3,ltime=.05,end='\n'): # the basic sprint function; prints letter by letter
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        if letter == "." or letter == "!" or letter == "?":
+            time.sleep(ptime)
+        elif letter == ",":
+            time.sleep(ctime)
+        else:
+            time.sleep(ltime)
+    sys.stdout.write(end)
+    sys.stdout.flush()
+    return ''
+sprint('What do you want the maximum number of minutes to be jumpscared to be?',end='')
+minutes = input()
+seconds = random.randrange(0,60*minutes)
+sprint('You will be jumpscared any second in the next {num_of_min} minutes, starting...')
+print('Now!')
+start_time = time.time()
+num = 0
+while time.time() < start_time + seconds:
+  time.sleep(1)
+  num += 1
+  print(num)?
+time.sleep(.5)
+print('Boo!')
+time.sleep(1)
+sprint('Were you scared?')
